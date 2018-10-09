a68409d27fea0ac58199bf88c4644bb939828597
luko <luko.web@gmail.com>
2018-10-09 18:26:39 -0300

    first commit

---
+++ Checklist/ChecklistViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ChecklistViewController.swift
+//  Checklist
+//
+//  Created by Luciano Schillagi on 09/10/2018.
+//  Copyright © 2018 luko. All rights reserved.
+//
+
+/* Controller */
+
+import UIKit
+
+/* Abstract:
+Una pantalla con un listado de películas ordenadas por categorías. También contiene un buscador para filtrar por nombre.
+*/
+
+class ChecklistViewController: UIViewController {
+
+//*****************************************************************
+// MARK: - Properties
+//*****************************************************************
+
+
+}
